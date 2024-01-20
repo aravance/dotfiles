44d59d125a7541695e64b2414efad3651dbda179
Alex Avance <aravance@gmail.com>
2024-01-20 14:32:13 -0600

    Add wofi

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-dirs=(hypr waybar tmux git alacritty)
+dirs=(hypr waybar tmux git alacritty wofi)
 
 for d in "${dirs[@]}";do
   ln -s "${SCRIPT_DIR}/$d" ~/.config 2>/dev/null
