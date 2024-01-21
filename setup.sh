b2d2c3a534e3dce34df9fd481794c16529801867
Alex Avance <aravance@gmail.com>
2024-01-20 20:59:04 -0600

    Add keyd

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-dirs=(hypr waybar tmux git alacritty wofi)
+dirs=(hypr waybar tmux git alacritty wofi keyd)
 
 for d in "${dirs[@]}";do
   ln -s "${SCRIPT_DIR}/$d" ~/.config 2>/dev/null
