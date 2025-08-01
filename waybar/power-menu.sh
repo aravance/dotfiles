162c233db56b96132c010a6f2cb5d64a11711dd8
Alex Avance <aravance@gmail.com>
2025-08-01 09:17:40 -0500

    fix(waybar): fix power button size and styling

--- waybar/power-menu.sh
+++ waybar/power-menu.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-entries="⏻ Shutdown\n⭮ Reboot\n⇠ Logout\n⏾ Suspend"
+shutdown='<span size="x-large">⏻</span> Shutdown'
+reboot='<span>⭮</span> Reboot'
+logout='<span>⇠</span> Logout'
+suspend='<span>⏾</span> Suspend'
+entries="$shutdown\n$reboot\n$logout\n$suspend"
 
 # there's a bug in wofi-1.5 causing -L to fail for wofi, set height directly for now -L 4
-selected=$(echo -e $entries | wofi --width 140 --height 205 -p "" -D single_click=true -i --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+selected=$(echo -e $entries | wofi --allow-markup --width 220 --height 250 -p "" -D single_click=true -i --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
   shutdown)
